9a6ba75d0c510327576f479db48a66026b6dba78
sohan2410 <bandarysohan24@gmail.com>
2023-01-15 20:44:45 +0530

    setup

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const bodyParser = require("body-parser")
+const db = require("./configs/db")
+
+const port = process.env.PORT || 8000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(cors())
+
+// app.use('/api/auth', require('./routes/auth'))
+
+app.get("/", (_, res) => {
+  res.send("Server is listening for requests")
+})
+
+app.use(require("./middlewares/exceptions/handler"))
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+db()
