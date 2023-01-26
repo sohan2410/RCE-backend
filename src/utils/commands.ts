650f4e7629221e011b342b8d0e86294c693510b5
shivam saraswat <shivam@gmail.com>
2023-01-27 00:35:11 +0530

    input added

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -5,11 +5,11 @@ export const command = (dir: string, fileName: string, format: string): string =
     case "java":
       return `cd ${dir} && java ${fileName}.java`
     case "py":
-      return `cd ${dir} && python ${fileName}.py`
+      return `cd ${dir} && python ${fileName}.py ${fileName}.txt`
     case "c":
-      return `cd ${dir} && gcc ${fileName}.c -o a.out && bash -c "./a.out"`
+      return `cd ${dir} && gcc ${fileName}.c -o a.out && bash -c "./a.out < ${fileName}.txt"`
     case "cpp":
-      return `cd ${dir} && g++ ${fileName}.cpp -o a.out && bash -c "./a.out"`
+      return `cd ${dir} && g++ ${fileName}.cpp -o a.out && bash -c "./a.out < ${fileName}.txt"`
     default:
       const err = new Error(`command not found: ${fileName}`)
       err.name = "CommandError"
