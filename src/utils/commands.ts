6eb87ba21d2ad8d85ab90b1be4c6ea8da76137de
sohan2410 <bandarysohan24@gmail.com>
2023-02-15 13:26:40 +0530

    added bin files

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,14 +1,15 @@
 export const command = (dir: string, fileName: string, format: string): string => {
   console.log("commands", dir, fileName, format)
   switch (format) {
-    case "js":
+    case "js": //done
       return `cd ${dir} && node ${fileName}.js < ${fileName}.txt`
-    case "java":
-      return `cd ${dir} && java ${fileName}.java < ${fileName}.txt`
-    case "py":
-      return `cd ${dir} && bash -c "python ${fileName}.py < ${fileName}.txt"`
+    case "java": //done
+      return `cd ./bin && bash -c "./java /tmp/${fileName}.java < /tmp/${fileName}.txt"`
+    case "py": //done
+      return `cd ./bin && bash -c "./python3.8 /tmp/${fileName}.py < /tmp/${fileName}.txt"`
     case "c":
-      return `cd ${dir} && gcc ${fileName}.c -o a.out && bash -c "./a.out < ${fileName}.txt"`
+      return `cd ./bin && bash -c "./gcc /tmp/${fileName}.c -o a.out" && bash -c "./a.out < ${fileName}.txt"`
+    // return `cd ${dir} && gcc ${fileName}.c -o a.out && bash -c "./a.out < ${fileName}.txt"`
     case "cpp":
       return `cd ${dir} && g++ ${fileName}.cpp -o a.out && bash -c "./a.out < ${fileName}.txt"`
     default: {
